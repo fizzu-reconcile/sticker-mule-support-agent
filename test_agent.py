f53b65e52b653e05b46fc36ee3e662a93fe5a6c0
fizzu-reconcile <jatulute@gmail.com>
2026-09-23 22:45:47 +0100

    Add test for support_agent function

--- test_agent.py
+++ test_agent.py
@@ -3,3 +3,7 @@ from app import support_agent
 
 def test_support_agent_exists():
     assert callable(support_agent)
+
+
+def test_support_agent_accepts_message():
+    assert support_agent.__code__.co_argcount == 1
